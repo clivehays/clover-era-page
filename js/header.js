98b988acff6f9d6bd4f63198068da37dfa48c491
clivehays <clivehays@gmail.com>
2025-08-21 06:57:20 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -294,7 +294,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         <a href="/what-is-employee-engagement/">What Is Engagement?</a>
                         <a href="/#calculator">ROI Calculator</a>
                         <a href="/assessment/index.html">Free Assessment</a>
-                        <a href="/implementation-guide">Implementation Guide</a>
+                        <a href="/implementation-guide.html">Implementation Guide</a>
                         <a href="https://www.amazon.com/Trillion-Dollar-Problem-Employee-Engagement-ebook/dp/B0CYMC3ST1/ref=sr_1_1?crid=FIA8QK3LNQZ0&dib=eyJ2IjoiMSJ9.QImYjq6qcRR5M5fzSbzy1IVu2_5CiJxenm3uMu8aUcl0Zm3J_AVdoIA1WJCmWs-yufPlRkezuhnJurayQOLazLuWTn1XIlAntqSjdWmn-5yL0PHv6F6l-kA8t0O6EalS.nP4OcL7TscInMWUb8AW18JAlsmsQXTD-oY71REz59EU&dib_tag=se&keywords=The+trillion+Dollar+Problem&qid=1755545687&sprefix=the+trillion+dollar+problem%2Caps%2C258&sr=8-1" class="featured-link">
         📚 The Trillion Dollar Problem Book
     </a>
